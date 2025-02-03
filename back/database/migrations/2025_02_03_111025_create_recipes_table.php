43b1d8e400f9fc054f7409954cd9d2e5bea9e851
Iaas22 <ishaaamin22@gmail.com>
2025-02-03 12:31:30 +0100

    migraciones acabadas

--- back/database/migrations/2025_02_03_111025_create_recipes_table.php
+++ back/database/migrations/2025_02_03_111025_create_recipes_table.php
@@ -1,27 +1,33 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRecipesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('cuisine_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->json('ingredients');
+            $table->json('steps');
+            $table->string('image')->default(''); // O también puede ser ->nullable();
+            $table->integer('prep_time');
+            $table->integer('cook_time');
+            $table->integer('servings');
+            $table->json('nutrition')->nullable();
+            $table->integer('likes_count')->default(0); 
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('recipes');
     }
-};
+}
--- back/database/migrations/2025_02_03_111034_create_recipe_users_table.php
+++ back/database/migrations/2025_02_03_111034_create_recipe_users_table.php
@@ -1,27 +1,24 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateRecipeUserTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        Schema::create('recipe_users', function (Blueprint $table) {
+        Schema::create('recipe_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('recipe_id')->constrained()->onDelete('cascade'); // Esta línea es clave
+            $table->boolean('saved')->default(false);
+            $table->boolean('liked')->default(false); // Indica si el usuario ha dado like
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('recipe_users');
+        Schema::dropIfExists('recipe_user');
     }
-};
+}
