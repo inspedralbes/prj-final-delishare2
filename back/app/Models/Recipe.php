1b52b706c3e12cb29b69f2dbfc16cd61e713a00a
Iaas22 <ishaaamin22@gmail.com>
2025-02-03 12:13:31 +0100

    modelos creados

---
+++ back/app/Models/Recipe.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'category_id', 'cuisine_id', 'title', 'description', 
+        'ingredients', 'steps', 'image', 'prep_time', 'cook_time', 
+        'servings', 'nutrition'
+    ];
+
+    protected $casts = [
+        'ingredients' => 'array',
+        'steps' => 'array',
+        'nutrition' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cuisine()
+    {
+        return $this->belongsTo(Cuisine::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'recipe_user')
+                    ->withPivot('saved', 'liked')
+                    ->withTimestamps();
+    }
+    public function usersWhoSaved()
+{
+    return $this->belongsToMany(User::class, 'recipe_user')->withPivot('saved', 'liked')->where('saved', true);
+}
+
+    
+}
