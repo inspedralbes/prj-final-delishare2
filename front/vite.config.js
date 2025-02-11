9091f34389a0f67a7017172754a784cf8143699f
simrankaurrrr <a23gurkaukau@inspedralbes.cat>
2025-02-11 09:06:07 +0100

    docker va bien

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -13,6 +12,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
+  },
+  server: {
+    host: '0.0.0.0', 
+    port: 5173,
   },
 })
